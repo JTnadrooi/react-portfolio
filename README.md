16e8bdd2a6dea121dc2bbdb103245579707b68cd
JTnadrooi <jtnadrooi@gmail.com>
2025-03-14 11:21:37 +0100

    added scss functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+
+My portfolio, created with React and SASS/SCSS!
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				
+
 			</header>
 		</div>
 	);
