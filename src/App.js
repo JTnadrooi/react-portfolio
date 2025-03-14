6bdcad48c44d70aca45f0b78687674865c06e9ab
JTnadrooi <jtnadrooi@gmail.com>
2025-03-14 12:33:27 +0100

    media and fonts added, ported from the html portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.scss';
+import './scss/App.scss';
+// import './App.scss';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-
+				hi
 			</header>
 		</div>
 	);
